664b8e6ff15387e8c583c8e4003b30b1d83ef192
Ata Mohammadi <atasmohammadi@gmail.com>
2026-09-11 13:43:51 +0300

    fix: point apiBaseURL to production hushtunnel.com by default

--- ApplicationLibrary/HushTunnel/Core/BrandConfig.swift
+++ ApplicationLibrary/HushTunnel/Core/BrandConfig.swift
@@ -2,11 +2,12 @@ import Foundation
 
 public enum BrandConfig {
     public static let appName = "HushTunnel"
-    #if DEBUG && targetEnvironment(simulator)
-    public static let apiBaseURL = "http://localhost:3003"
-    #else
-    public static let apiBaseURL = "https://www.hushtunnel.com"
-    #endif
+    public static var apiBaseURL: String {
+        if let override = ProcessInfo.processInfo.environment["HUSH_API_BASE_URL"], !override.isEmpty {
+            return override
+        }
+        return "https://www.hushtunnel.com"
+    }
     public static let appScheme = "hushtunnel"
     public static let supportURL = "https://www.hushtunnel.com"
     public static let privacyURL = "https://www.hushtunnel.com/privacy"
